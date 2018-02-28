de38fb1fb785a205b5b319e08174a393b75c884a
Naila Ferrante <nailaferrante@hotmail.com>
2018-02-28 19:38:24 -0300

    exclusao

--- hearme-backend.domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace hearme_backend.domain
-{
-    public class Class1
-    {
-    }
-}
--- hearme-backend.repository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace hearme_backend.repository
-{
-    public class Class1
-    {
-    }
-}
