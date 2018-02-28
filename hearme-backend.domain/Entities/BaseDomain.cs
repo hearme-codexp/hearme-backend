70c711a293205208c2093c676e792640847b00e6
Naila Ferrante <nailaferrante@hotmail.com>
2018-02-28 20:43:22 -0300

    Atualização BaseDomain e ClientesDomain com inclusão de DataAnnotations

--- hearme-backend.domain/Entities/BaseDomain.cs
+++ hearme-backend.domain/Entities/BaseDomain.cs
@@ -1,8 +1,12 @@
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace hearme_backend.domain.Entities
 {
     public class BaseDomain
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
     }
 }
--- hearme-backend.domain/Entities/ClientesDomain.cs
+++ hearme-backend.domain/Entities/ClientesDomain.cs
@@ -1,11 +1,31 @@
 
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
 namespace hearme_backend.domain.Entities
 {
     public class ClientesDomain : BaseDomain
     {
+        [Required]
+        [StringLength(100)]
         string Nome {get; set;}
+
+        [Required]
+        [StringLength(20)]
         Genero Genero {get; set;}
+
+        [Required]
+        [DataType(DataType.DateTime)]
         DateTime DataNascimento {get; set;}
+
+        [Required]
+        [StringLength(20)]
         GrauDeficiencia GrauDeficiencia {get; set;}
+        
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime DataCriacao { get; set; }
     }
 }
