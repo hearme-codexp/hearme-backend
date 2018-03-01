0ba4af19bdc5feb4cea0ed8fcb8c8cb5ee3191fa
unknown <edusousa59@gmail.com>
2018-02-28 21:35:35 -0300

    Início do Repositório

---
+++ hearme-backend.repository/Repositories/BaseRepository.cs
@@ -0,0 +1,9 @@
+using hearme_backend.domain.Contracts;
+
+namespace hearme_backend.repository.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+
+    }
+}
