ee899499db5f6abd26b36bf94735252c24e34f70
unknown <edusousa59@gmail.com>
2018-03-01 20:01:15 -0300

    Contexto Finalizado

--- hearme-backend.repository/Context/HearMeContext.cs
+++ hearme-backend.repository/Context/HearMeContext.cs
@@ -1,7 +1,27 @@
+using hearme_backend.domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
 namespace hearme_backend.repository.Context
 {
-    public class HearMeContext
+    public class HearMeContext : DbContext
     {
+        public HearMeContext(DbContextOptions<HearMeContext> options) : base(options)
+        {
         
+        }
+
+        public DbSet<ClientesDomain> Clientes {get; set;}
+        public DbSet<GeneroDomain> Generos {get; set;}
+        public DbSet<GrauDeficienciaDomain> GrausDeficiencias {get; set;}
+        public DbSet<UsuarioDomain> Usuarios {get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            modelBuilder.Entity<ClientesDomain>().ToTable("Clientes");
+            modelBuilder.Entity<GeneroDomain>().ToTable("Generos");
+            modelBuilder.Entity<GrauDeficienciaDomain>().ToTable("GrausDeficiencias");
+            modelBuilder.Entity<UsuarioDomain>().ToTable("Usuarios");
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
