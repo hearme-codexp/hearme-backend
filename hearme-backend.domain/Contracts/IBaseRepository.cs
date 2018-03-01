0c952579f0dd4b6d120360dbbe52f270ce064a10
unknown <edusousa59@gmail.com>
2018-02-28 21:34:23 -0300

    Interface Finalizada

--- hearme-backend.domain/Contracts/IBaseRepository.cs
+++ hearme-backend.domain/Contracts/IBaseRepository.cs
@@ -2,12 +2,12 @@ using System.Collections.Generic;
 
 namespace hearme_backend.domain.Contracts
 {
-    public interface IBaseRepository<T>
+    public interface IBaseRepository<T> where T : class
     {
         IEnumerable<T> Listar(string[] includes = null);
         int Atualizar(T dados);
         int Inserir(T dados);
-        T Buscar(int id, string[] includes = null)
+        T Buscar(int id, string[] includes = null);
 
     }
 }
