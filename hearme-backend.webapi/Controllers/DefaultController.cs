36d133edc692a75a606ce76f6d3604044653e116
Eduardo Lima <duuhlewis@macs-MacBook-Pro.local>
2018-03-20 16:45:14 -0300

    Default Controller para redirecionamento de páginas

---
+++ hearme-backend.webapi/Controllers/DefaultController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace hearme_backend.webapi.Controllers
+{
+    public class DefaultController : Controller
+    {
+        [Route(""), HttpGet]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public RedirectResult RedirectToSwaggerUi()
+        {
+            return Redirect("/swagger/");
+        }
+    }
+}
